d410ea9f8f8f339e15a059b7e8ee3ca8ce0f3c15
thiago123789 <thiago123789@gmail.com>
2019-02-28 18:15:38 -0300

    update code relation slides

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Slide:
 pictures = []
 slides = []
 
-file_name = "e_shiny_selfies.txt"
+file_name = "c_memorable_moments.txt"
 
 
 def read_file():
@@ -59,30 +59,54 @@ def create_slides():
             slides.append(slide)
 
 
-def create_relation():
-    for i in range(0, len(slides), 2):
-        if i + 1 < len(slides):
-            slide1 = slides[i].tags
-            slide2 = slides[i+1].tags
-            intersec = len(slide1.intersection(slide2))
-            sld1sld2 = len(slide1.difference(slide2))
-            sld2sld1 = len(slide2.difference(slide1))
-            value_min = min(intersec, sld1sld2, sld2sld1)
+def calculate_min(slide1, slide2):
+    if slide1 is not None and slide2 is not None:
+        slide1 = slide1.tags
+        slide2 = slide2.tags
+        intersec = len(slide1.intersection(slide2))
+        sld1sld2 = len(slide1.difference(slide2))
+        sld2sld1 = len(slide2.difference(slide1))
+        value_min = min(intersec, sld1sld2, sld2sld1)
+        return value_min
+    else:
+        return 0
+
+
+list_final = []
+
+
+def calculate_relation():
+    max_min = 0
+    index_max_min = -1
+    list_final.append(slides[0])
+    del slides[0];
+    while len(slides) != 0:
+        for sld in list(slides):
+            min = calculate_min(sld, list_final[-1])
+            if min > max_min:
+                max_min = min
+                index_max_min = sld
+        print("Length: "+ str(len(slides)))
+        print(index_max_min)
+        slides.remove(sld)
+        list_final.append(sld)
 
 
 def write_file():
     output_name = "output_"+file_name
     file = open(output_name, "w")
-    file.writelines(str(len(slides))+"\n")
-    for i in slides:
-        line = ""
-        for p in i.pictures:
-            line += str(p.id) + " "
-        file.writelines(line+"\n")
+    file.writelines(str(len(list_final))+"\n")
+    for i in list_final:
+        if i is not None:
+            line = ""
+            for p in i.pictures:
+                line += str(p.id) + " "
+            file.writelines(line+"\n")
     file.close()
 
 
 read_file()
 create_slides()
-create_relation()
+calculate_relation()
+print(len(list_final))
 write_file()
